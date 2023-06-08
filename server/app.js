7e84fc66db97faeb4112dc95d3f74ff49df90288
Martin Tomov <devsonspree@gmail.com>
2023-06-09 02:37:10 +0800

    fix server file to deploy

--- server/app.js
+++ server/app.js
@@ -1,42 +1,45 @@
-require('dotenv').config();
-require('./db/index');
-const express = require('express');
-const bodyParser = require('body-parser');
-const flash = require('express-flash');
-const session = require('express-session');
-const cors = require('cors');
-const path = require('path');
-const helmet = require('helmet');
-const multer = require('multer');
+require("dotenv").config();
+require("./db/index");
+const express = require("express");
+const bodyParser = require("body-parser");
+const flash = require("express-flash");
+const session = require("express-session");
+const cors = require("cors");
+const path = require("path");
+const helmet = require("helmet");
+const multer = require("multer");
 const app = express();
 
-const service = require('./service.js');
-const util = require('./util.js');
+const service = require("./service.js");
+const util = require("./util.js");
 
-const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/login');
-const passcodeRouter = require('./routes/passcode');
+const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
+const passcodeRouter = require("./routes/passcode");
 // var dataRouter = require('./routes/data');
-const cron = require('node-cron');
-const so_scraper = require('../scrapers/so_scraper');
-const vsin_scraper = require('../scrapers/vsin_scraper');
-const yahoo_scraper = require('../scrapers/yahoo_scraper');
+const cron = require("node-cron");
+const so_scraper = require("../scrapers/so_scraper");
+const vsin_scraper = require("../scrapers/vsin_scraper");
+const yahoo_scraper = require("../scrapers/yahoo_scraper");
 
-app.use(express.static('assets'));
-app.use('/images', express.static('images'));
+app.use(express.static("assets"));
+app.use("/images", express.static("images"));
 
 // Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(session({
-  cookie: { maxAge: 3600000 },
-  store: new session.MemoryStore,
-  saveUninitialized: true,
-  resave: 'true',
-  secret: 'secret'
-}))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(
+  session({
+    cookie: { maxAge: 3600000 },
+    store: new session.MemoryStore(),
+    saveUninitialized: true,
+    resave: "true",
+    secret: "secret",
+  })
+);
 
 app.use(flash());
 
@@ -44,13 +47,13 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      'default-src': ["'self'", 'data:', "'unsafe-inline'", "'unsafe-eval'"],
-      'img-src': ["'self'", 'https: data:'],
-      'script-src': [
+      "default-src": ["'self'", "data:", "'unsafe-inline'", "'unsafe-eval'"],
+      "img-src": ["'self'", "https: data:"],
+      "script-src": [
         "'self'",
         "'unsafe-inline'",
         "'unsafe-eval'",
-        'www.google-analytics.com',
+        "www.google-analytics.com",
       ],
     },
   })
@@ -58,28 +61,28 @@ app.use(
 
 const isAuthenticated = (req, res, next) => {
   if (req.session.token) {
-    console.log('token>>', req.session.token);
+    console.log("token>>", req.session.token);
     // User is authenticated, call next() to proceed to the next middleware
     return next();
   }
 
   // User is not authenticated, redirect to login page
-  res.redirect('/');
+  res.redirect("/");
 };
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/', loginRouter);
+app.use("/", loginRouter);
 
-app.use('/users', isAuthenticated, usersRouter);
-app.use('/passcode', isAuthenticated, passcodeRouter);
+app.use("/users", isAuthenticated, usersRouter);
+app.use("/passcode", isAuthenticated, passcodeRouter);
 // app.use('/data', dataRouter);
 // app.use('/howtouse', dataRouter);
 
 const PORT = process.env.PORT || 3001;
-const DIR = './assets/images/';
+const DIR = "./assets/images/";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -91,45 +94,57 @@ const storage = multer.diskStorage({
   },
 });
 
-app.get('/api/getAggregatedData', (req, res) => {
+app.get("/api/getAggregatedData", (req, res) => {
   let sport = req.query.sport;
-  if (sport == null || sport == '') res.status(400).send('Invalid Sport');
-  service.getAggregatedData(sport).then(
-    (data) => {
-      res.status(200).send(util.getResponse(data, true));
-    },
-    (err) => {
-      res.status(500).send(util.getResponse(err, false));
-    }
-  ).catch((err) => res.status(403).send(util.getResponse(err, false)));
+  if (sport == null || sport == "") res.status(400).send("Invalid Sport");
+  service
+    .getAggregatedData(sport)
+    .then(
+      (data) => {
+        res.status(200).send(util.getResponse(data, true));
+      },
+      (err) => {
+        res.status(500).send(util.getResponse(err, false));
+      }
+    )
+    .catch((err) => res.status(403).send(util.getResponse(err, false)));
 });
 
-app.post('/api/subscribe', (req, res) => {
-  service.subscribe(req.body).then(
-    (data) => {
-      res.status(200).send({ message: 'success' });
-    },
-    (err) => {
-      res.status(500).send({ message: 'Duplicated Userinfo!', passcode: 'ABCD' });
-    }
-  ).catch((err) => res.status(403).send(util.getResponse(err, false)));
+app.post("/api/subscribe", (req, res) => {
+  service
+    .subscribe(req.body)
+    .then(
+      (data) => {
+        res.status(200).send({ message: "success" });
+      },
+      (err) => {
+        res
+          .status(500)
+          .send({ message: "Duplicated Userinfo!", passcode: "ABCD" });
+      }
+    )
+    .catch((err) => res.status(403).send(util.getResponse(err, false)));
 });
 
-app.get('/api/passcodeLogin', (req, res) => {
+app.get("/api/passcodeLogin", (req, res) => {
   let passcode = req.query.passcode;
-  service.passcodeLogin(passcode).then(
-    (data) => {
-      res.status(200).send({ data });
-    },
-    (err) => {
-      res.status(500).send({ message: 'Wrong Passcode!' });
-    }
-  ).catch((err) => res.status(403).send(util.getResponse(err, false)));
+  service
+    .passcodeLogin(passcode)
+    .then(
+      (data) => {
+        res.status(200).send({ data });
+      },
+      (err) => {
+        res.status(500).send({ message: "Wrong Passcode!" });
+      }
+    )
+    .catch((err) => res.status(403).send(util.getResponse(err, false)));
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  // res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
@@ -140,7 +155,7 @@ app.listen(PORT, () => {
 const config = {
   server: process.env.DATABASE_SERVER,
   authentication: {
-    type: 'default',
+    type: "default",
     options: {
       userName: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
@@ -153,7 +168,7 @@ const config = {
 };
 
 // Set cron job for the scrapers
-cron.schedule('0 */20 * * * *', () => {
+cron.schedule("0 */20 * * * *", () => {
   const scheduler_mins = new Date().getMinutes();
   if (scheduler_mins >= 0 && scheduler_mins < 20) {
     yahoo_scraper.yahoo_scraper(config);
