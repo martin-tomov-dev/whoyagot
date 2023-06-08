c8bdc98ca929c775b2986a0a6174ead015e70d04
Martin Tomov <devsonspree@gmail.com>
2023-06-09 04:01:51 +0800

    test2

--- build1/routes/data.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// display user page
-router.get('/', function (req, res, next) {
-    res.render('data', { data: [] });
-});
-
-module.exports = router;
