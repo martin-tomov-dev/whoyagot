5aaaafbbff2088a3f155ed2ba3eb9f51626faedb
Martin Tomov <devsonspree@gmail.com>
2023-06-08 21:39:07 +0800

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,33 @@
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 import "react-toastify/dist/ReactToastify.css";
-import './App.css';
-import { FilterContextProvider } from './context/FilterContext';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import Main from './pages/Main';
-import DataPage from './pages/DataPage';
-import HowToUse from './pages/HowToUse';
-import withAuth from './hooks/withAuth'
+import "./App.css";
+import { FilterContextProvider } from "./context/FilterContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Main from "./pages/Main";
+import DataPage from "./pages/DataPage";
+// import HowToUse from "./pages/HowToUse";
+import withAuth from "./hooks/withAuth";
 
 function App() {
   const AuthDataPage = withAuth(DataPage);
-  const AuthHowToUse = withAuth(HowToUse);
+  // const AuthHowToUse = withAuth(HowToUse);
 
   return (
     <>
       <BrowserRouter>
-        <ToastContainer position='top-right' />
+        <ToastContainer position="top-right" />
         <Routes>
           <Route index element={<Main />} />
-          <Route path='/page-three' element={<AuthHowToUse />} />
-          <Route path='/data' element={
-            <FilterContextProvider>
-              <AuthDataPage />
-            </FilterContextProvider>
-          } />
+          {/* <Route path="/page-three" element={<AuthHowToUse />} /> */}
+          <Route
+            path="/data"
+            element={
+              <FilterContextProvider>
+                <AuthDataPage />
+              </FilterContextProvider>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
